1deaa18bc49fcdc1fedc0120dc952002f4406d68
hanyaping3 <957706736@qq.com>
2021-01-28 00:29:36 +0800

    这是s静态首页，详情页，商品列表“

---
+++ .history/gulpfile_20210127233920.js
@@ -0,0 +1,50 @@
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const minifyCss = require("gulp-minify-css");
+
+let devPath = "./src/";
+let pusblishPath = "D:\\PHPnow-1.5.6\htdocs";
+
+// 复制html文件
+function copyHtml(){
+    console.log("copyHtml");
+    gulp.src(devPath+"*.html")
+    .pipe(gulp.dest(pusblishPath));
+}
+
+// 复制图片文件
+function copyImg(){
+    gulp.src(devPath+"img/*.{jpg,png}")
+    .pipe(gulp.dest(pusblishPath+"\\img"));    
+}
+
+function copyPhp(){
+    gulp.src(devPath+"php/**/*")
+    .pipe(gulp.dest(pusblishPath+"\\php"));
+}
+
+// 处理sass（把sass编译成css）
+function doSass(){
+    gulp.src(devPath+"scss/*.scss")
+    .pipe(sass())
+    .pipe(minifyCss())
+    .pipe(gulp.dest(pusblishPath+"\\css"));
+}
+
+// 处理js（压缩）
+function doJS(){
+    gulp.src(devPath+"js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest(pusblishPath+"\\js"));
+}
+
+gulp.task("watchall",async ()=>{
+    
+    gulp.watch(devPath+"*.html",async ()=>{copyHtml()});
+    gulp.watch(devPath+"img/*.{jpg,png}",async ()=>{copyImg()});
+    gulp.watch(devPath+"scss/*.scss",async ()=>{doSass()});
+    gulp.watch(devPath+"js/*.js",async ()=>{doJS()});
+    gulp.watch(devPath+"php/**/*",async ()=>{copyPhp()});
+
+});
